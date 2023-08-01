23f8a7c49d676f1e307a71ab75f648d506fc0393
Camila Haas Primieri <camilahp@unops.org>
2023-08-01 15:32:38 +0200

    Set up CI/CD deployment and small fixes

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    baseUrlAPI: 'pmuportalservice_url', // PMU Portal Service URL
+    baseUrlAPIDocUploadAPI:
+      'pmuportalservice_url + /document/upload2', // PMU Portal Service URL + /document/upload2
+    baseUrlAPIDocdownloadAPI:
+      'pmuportalservice_url + /document/downloadDocument', // PMU Portal Service URL + /document/downloadDocument
+    baseSyncAPI: 'pmuscheduler_url', // PMU Scheduler URL
+    baseUrlCountryAPI: 'countryportalservice_url', // Country Portal Service URL
+    countryServiceUrl: 'pmuportalservice_url + /country-profile', // PMU Portal Service URL + /country-profile
+    countryWebUrl: 'countryportalweb_url + /landing-page', // Country Portal Web URL + /landing-page
+  };
+  
