30a4fd3393bf1fe0e70acf17543b8483ba7c69a8
Camila Haas Primieri <camilahp@unops.org>
2023-08-01 16:56:04 +0200

    Bug fix

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  baseUrlAPI: 'https://icat-pmuportalservice-qa-2sshj5de3a-ey.a.run.app', // PMU Portal Service URL
+  baseUrlAPIDocUploadAPI:
+    'https://icat-pmuportalservice-qa-2sshj5de3a-ey.a.run.app/document/upload2', // PMU Portal Service URL + /document/upload2
+  baseUrlAPIDocdownloadAPI:
+    'https://icat-pmuportalservice-qa-2sshj5de3a-ey.a.run.app/document/downloadDocument', // PMU Portal Service URL + /document/downloadDocument
+  baseSyncAPI: 'https://icat-pmuscheduler-qa-2sshj5de3a-ey.a.run.app', // PMU Scheduler URL
+  baseUrlCountryAPI: 'https://icat-countryportalservice-qa-2sshj5de3a-ey.a.run.app', // Country Portal Service URL
+  countryServiceUrl: 'http://localhost:8083/country-profile', // PMU Portal Service URL + /country-profile
+  countryWebUrl: 'https://icat-countryportalweb-qa-2sshj5de3a-ey.a.run.app/landing-page', // Country Portal Web URL + /landing-page
+};
